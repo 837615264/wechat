d3c2f91407affa53bca0154b0f090f97214b05f0
837615264 <837615264@qq.com>
2020-07-03 03:48:02 +0800

    test

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -23,7 +23,8 @@ class Index
     public function getImg(){
         $wx = new Wx();
         $image = $wx->getMedia('V3HEw_0-Iie2kHNDfQpKVIg5tRhuq_uUifYiiwas8pOb5Arn9j4pFWWdB5AQNANa');
-        header('content-type:image/jpeg');
-        echo $image;
+        file_put_contents('./hotel.jpg',$image);
+        //header('content-type:image/jpeg');
+        //echo $image;
     }
 }
